0ebbf159437409d1f01541cc612855b9524fa965
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2020-12-01 16:52:26 +0100

    Add header to the United States GDP bar chart
    Also added the json file that will be supply the dataset for the bar chart

---
+++ js/app.js
@@ -0,0 +1,7 @@
+tippy("#more-info", {
+	content: `A Guide to the National Income and Product Accounts of the United States 
+			  (NIPA)<br /><a href="http://www.bea.gov/national/pdf/nipaguid.pdf" target="_blank">
+			  			http://www.bea.gov/national/pdf/nipaguid.pdf</a>`,
+	interactive: true,
+	allowHTML: true
+});
