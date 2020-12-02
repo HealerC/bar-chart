84508537c2f92d282e9f3380733a54abf4958e2d
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2020-12-02 22:02:21 +0100

    Add the axes

--- js/app.js
+++ js/app.js
@@ -30,7 +30,6 @@ function renderData(data) {
 					 .range([padding, width - padding])
 					 .padding(0.3)
 					 .round(true);
-
 	const yScale = d3.scaleLinear()
 					 .domain([0, d3.max(data.map(d => d[1]))])
 					 .range([height - padding, padding]);
@@ -42,5 +41,49 @@ function renderData(data) {
 	   .attr("x", (d) => xScale(d[0]))
 	   .attr("y", (d) => yScale(d[1]))
 	   .attr("width", xScale.bandwidth())
-	   .attr("height", (d) => height-padding - yScale(d[1]));
+	   .attr("height", (d) => height-padding - yScale(d[1]))
+
+	const { xg, yg } = renderAxis(svg, xScale, yScale, { height, padding });
+}
+
+function renderAxis(svg, xScale, yScale, dimensions) {
+	const { height, padding } = dimensions;
+
+	const xAxis = d3.axisBottom(xScale)
+						.tickValues(getTickValues(xScale))
+						.tickFormat(d => d.substring(0, 4));
+
+	const yAxis = d3.axisLeft(yScale);
+
+	const xg = svg.append("g")
+	   			  .attr("transform", `translate(0, ${height-padding})`)
+	   			  .call(xAxis);
+
+	const yg = svg.append("g")
+	   			  .attr("transform", `translate(${padding}, 0)`)
+	              .call(yAxis);
+
+	function getTickValues(scale) {
+		let filteredScale = [];
+
+		scale.domain().forEach(d => {
+			let value = +d.substring(0, 4);
+			if (value % 5 === 0 && !checkArray(filteredScale, value)) {
+				filteredScale.push(d);
+			}
+		});
+
+		return filteredScale;
+
+		function checkArray(arr, value) {
+			let re = new RegExp(value, "i");
+			for (let data in arr) {
+				if (re.test(arr[data])) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+	return { xg, yg }
 }
